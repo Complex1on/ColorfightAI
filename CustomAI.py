2a85f95200e57681953dc98780762e8e874dce97
Complexion <etyip@ucsb.edu>
2019-02-06 11:32:56 -0800

    random attack works

--- CustomAI.py
+++ CustomAI.py
@@ -5,6 +5,7 @@ import random
 
 def my_cells():
     #Goes through the board and finds user's cells
+    g.Refresh()
     mycells = []
     for x in range(g.width):
         for y in range(g.height):
@@ -12,21 +13,8 @@ def my_cells():
             if c.owner == g.uid:
                 mycells.append([x,y])
     #returns a list of users cells in list form of [0] = x , [1] = y
-    print(mycells)
+    #print(mycells)
     return mycells
-
-def attack_random(cells):
-    #picks one of my cells and a random cell next to it to attack
-    d = random.choice([[0,1], [0,-1], [1, 0], [-1,0]])
-    foundcell = False
-    while (foundcell == False):
-        g2 = random.choice(cells)
-        x = g2[0]+d[0] 
-        y = g2[1]+d[1] 
-        cc = g.GetCell(x,y)
-        if (cc != None) and (cc.owner != g.uid) and (cc.takeTime < 5):
-            foundcell = True
-    return [x, y]
 
 def surrounding_cells(mycells):
     direct = [[0,1], [0,-1], [1, 0], [-1,0]]
@@ -38,7 +26,7 @@ def surrounding_cells(mycells):
             cc = g.GetCell(x,y)
             if cc != None and cc.owner != g.uid:
                 adjacentcells.append([x,y])
-    print(adjacentcells)            
+    #print(adjacentcells)            
     return adjacentcells
             
 def lowest_atktime(adjcells):
@@ -51,17 +39,27 @@ def lowest_atktime(adjcells):
         c = g.GetCell(x ,y )
         c2 = g.GetCell(lowesttime[0],lowesttime[1]) 
         if (c.takeTime < c2.takeTime):
+            lowesttime = []
             lowesttime.append([x,y])
             
     print(lowesttime)
     return lowesttime
-            
-def randomroutine():
-    atk_target = attack_random(my_cells())
-    print(g.AttackCell(atk_target[0], atk_target[1]))
 
+def attack_random():
+    #picks one of my cells and a random cell next to it to attack
+    #picks random surrounding cell and attacks it
+    cell = random.choice(surrounding_cells(my_cells()))
+    cinfo = g.GetCell(cell[0],cell[1])
+    if cinfo.takeTime < 5:
+        print(g.AttackCell(cell[0],cell[1]))
 
- 
+def attack_lowest():
+    target = lowest_atktime(surrounding_cells(my_cells()))
+    print(g.AttackCell(target[0],target[1]))
+
+def defend_base():
+    #Need to implement defending a base
+    return 0
 
 
 
@@ -77,8 +75,9 @@ if __name__ == '__main__':
     if g.JoinGame('test'):
         print('hello')
         while True:
-            cell = lowest_atktime(surrounding_cells(my_cells()))
-            print(g.AttackCell(cell[0],cell[1]))
+            g.Refresh()
+            attack_random()
+            
         
     else:
         print("Failed to join the game!")
